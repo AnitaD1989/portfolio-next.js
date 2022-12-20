42acb80ca746a0c8f2a37d5d59d751643e7f8f02
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-12-20 12:36:34 +0100

    delete package-lock

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,4 +1,4 @@
-import { PageInfo } from "../typing.d";
+import { PageInfo } from "../typing";
 
 export const fetchPageInfo = async() => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
